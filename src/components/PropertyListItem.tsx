4bc57f6539f52576234321288b1dcb531086b4e7
markm10c <mark.mansour@m10c.com>
2026-07-23 13:56:27 +0100

    feedback

--- src/components/PropertyListItem.tsx
+++ src/components/PropertyListItem.tsx
@@ -1,3 +1,8 @@
+/**
+ * Adapted from https://github.com/devias-io/material-kit-react
+ * @license MIT License Copyright (c) 2021 Devias
+ */
+
 import type { FC } from 'react';
 import Box from '@mui/material/Box';
 import type { ListItemProps } from '@mui/material/ListItem';
